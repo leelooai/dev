e423b1b7c94d3728d447dcbd24aedc339451d90e
Maryna Pidoima <mpidoima@gmail.com>
2019-08-01 14:53:10 +0300

    Updates

---
+++ precache-manifest.04078322045cdac912b3fa5485d0d60f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42ee21bd1043a69fccc80e68a41f7d55",
+    "url": "/dev/index.html"
+  },
+  {
+    "revision": "6b685e5d13c554352fb4",
+    "url": "/dev/static/css/2.676dc204.chunk.css"
+  },
+  {
+    "revision": "c798fd7e769a362f9e80",
+    "url": "/dev/static/css/main.dadc716d.chunk.css"
+  },
+  {
+    "revision": "6b685e5d13c554352fb4",
+    "url": "/dev/static/js/2.09329ab4.chunk.js"
+  },
+  {
+    "revision": "c798fd7e769a362f9e80",
+    "url": "/dev/static/js/main.92f1c18a.chunk.js"
+  },
+  {
+    "revision": "7b18ca907f92e9a66def",
+    "url": "/dev/static/js/runtime~main.c0cef5d3.js"
+  },
+  {
+    "revision": "2afeea96bfeba9a86f27d764407bb52a",
+    "url": "/dev/static/media/blue_white_gorizontal.2afeea96.svg"
+  }
+]);
